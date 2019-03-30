47f00ea3d66ffe36d6c0172fed83c5b786bd91ae
shaitanshamma <kurbatov_andr@hotmail.com>
2019-03-30 11:04:57 +0300

    Пробная версия

--- Arrays2.java
+++
@@ -1,9 +0,0 @@
-public class Arrays2 {
-    public static void main(String[] args) {
-        int [] arr = new int [8];
-        for (int i = 0; i < arr.length; i++){
-            arr[i] = i * 3;
-            System.out.print(arr[i] + " ");
-        }
-    }
-}
